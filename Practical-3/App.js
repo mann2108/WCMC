a2bc6dc02857f7fb2f0a51ac670140b2a76b4e7e
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-02-27 01:42:20 +0530

    Update App.js

--- Practical-3/App.js
+++ Practical-3/App.js
@@ -12,9 +12,6 @@ import {
 } from "react-native";
 
 function getTime() {
-  // var date = new Date().getDate(); //Current Date
-  // var month = new Date().getMonth() + 1; //Current Month
-  // var year = new Date().getFullYear(); //Current Year
   var hours = new Date().getHours(); //Current Hours
   var min = new Date().getMinutes(); //Current Minutes
   var sec = new Date().getSeconds(); //Current Seconds
